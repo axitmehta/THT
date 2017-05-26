e3ba61ec1adc272a37315f2746b01fbd237e6015
Karthik Mohan <karthik.m@chikoop.com>
2017-05-26 12:04:20 +0530

    test1

--- app/src/main/java/talkhappytherapy/com/tht/MainActivity.java
+++ app/src/main/java/talkhappytherapy/com/tht/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -26,6 +27,11 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -128,8 +134,30 @@ public class MainActivity extends AppCompatActivity
 
             });
 
+            read_from_db();
         navigationView.setNavigationItemSelectedListener(this);
         }
+    }
+
+    void read_from_db()
+    {
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("events");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.child("events").getValue(String.class);
+                Log.d("TAG", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     @Override
@@ -170,17 +198,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_become_volunteer) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sponser) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_support) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_faq) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contact) {
 
         }
 
